fb83f512d6f15767f44b6d485147e72c051828f0
Juan Carlos Duran Solorzano <juank060790@gmail.com>
2022-05-06 16:54:48 -0500

    video mobile fixed

--- src/components/Header/CoverHeader.jsx
+++ src/components/Header/CoverHeader.jsx
@@ -10,10 +10,10 @@ const CoverHeader = () => {
   const [videoPlay, setVideoPlay] = React.useState(false);
   const vidRef = useRef(null);
 
-  const handlePlayVideo = () => {
-    vidRef.current.play();
-    console.log("videoRef :>> ", vidRef.current.play());
-  };
+  // const handlePlayVideo = () => {
+  //   vidRef.current.play();
+  //   console.log("videoRef :>> ", vidRef.current.play());
+  // };
   // function isMobile() {
   //   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
   //     navigator.userAgent
@@ -85,14 +85,16 @@ const CoverHeader = () => {
           </span>
         </div>
       </div>
-      <div onClick={handlePlayVideo} className="video-icon">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-          <path
-            fill="#ffffff"
-            d="M336.2 64H47.8C21.4 64 0 85.4 0 111.8v288.4C0 426.6 21.4 448 47.8 448h288.4c26.4 0 47.8-21.4 47.8-47.8V111.8c0-26.4-21.4-47.8-47.8-47.8zm189.4 37.7L416 177.3v157.4l109.6 75.5c21.2 14.6 50.4-.3 50.4-25.8V127.5c0-25.4-29.1-40.4-50.4-25.8z"
-          />
-        </svg>
-      </div>
+      <a href="https://youtu.be/cl7gG2MCs04" target="_new">
+        <div className="video-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path
+              fill="#ffffff"
+              d="M336.2 64H47.8C21.4 64 0 85.4 0 111.8v288.4C0 426.6 21.4 448 47.8 448h288.4c26.4 0 47.8-21.4 47.8-47.8V111.8c0-26.4-21.4-47.8-47.8-47.8zm189.4 37.7L416 177.3v157.4l109.6 75.5c21.2 14.6 50.4-.3 50.4-25.8V127.5c0-25.4-29.1-40.4-50.4-25.8z"
+            />
+          </svg>
+        </div>
+      </a>
     </div>
   );
 };
